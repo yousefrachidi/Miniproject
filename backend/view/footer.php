10407b7ac865b10e54b526f8e42d72a8be7b345e
youssefrachidi <yourath3@gmail.com>
2021-04-27 02:06:32 +0000

    first commit

---
+++ backend/view/footer.php
@@ -0,0 +1,15 @@
+<script type="text/javascript">
+    $(document).ready(function(){
+      $('.nav_btn').click(function(){
+        $('.mobile_nav_items').toggleClass('active');
+      });
+ 
+    });
+    </script>
+
+  <script src="../js/jquery-3.5.1.slim.min.js"></script>
+  <script src="../js/popper.min.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
+
+ </body>
+</html>
